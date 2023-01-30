850534ab0ed9680e65120c2565fccf0913aebd78
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2023-01-30 11:15:36 -0800

    tried to see if np.clip() works, and it does

--- Code/src/plotCreator.py
+++ Code/src/plotCreator.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 def valueHistogram(array):
     #this function sucks
-    flattenArray = [x for x in array.flatten("C") if not x < 1.0]
+    flattenArray = [x for x in array.flatten("C")]
     plt.hist(x=flattenArray, bins='auto')
 
 def standardPlot(array):
